b2f5647092a0658d2408b083ed3acead20cd9559
4sploit <4splo17@gmail.com>
2024-05-20 00:07:26 +0300

    apply inheritance mapping on JPA entities

---
+++ src/main/java/io/github/sploit/projects/taskmanager/common/entities/BaseEntity.java
@@ -0,0 +1,31 @@
+package io.github.sploit.projects.taskmanager.common.entities;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@MappedSuperclass
+public class BaseEntity implements Serializable {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+    @Column(name = "created_at", updatable = false)
+    @DateTimeFormat(pattern = "dd-MM-yyyy")
+    private LocalDate createDate;
+
+    @Column(name = "updated_at")
+    @DateTimeFormat(pattern = "dd-MM-yyyy")
+    private LocalDate updateDate;
+}
---
+++ src/main/java/io/github/sploit/projects/taskmanager/common/entities/TitledEntity.java
@@ -0,0 +1,16 @@
+package io.github.sploit.projects.taskmanager.common.entities;
+
+import io.micrometer.common.lang.NonNull;
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@MappedSuperclass
+public class TitledEntity extends BaseEntity {
+    @NonNull
+    @Column(name = "title")
+	private String title;
+}
