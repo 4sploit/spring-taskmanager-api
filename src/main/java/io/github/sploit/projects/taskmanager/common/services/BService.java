756f19e5f3d593cb5e4a2309c412c271471ced8c
4sploit <4splo17@gmail.com>
2024-05-20 10:39:58 +0300

    bind business service type

--- src/main/java/io/github/sploit/projects/taskmanager/common/services/BService.java
+++ src/main/java/io/github/sploit/projects/taskmanager/common/services/BService.java
@@ -2,10 +2,12 @@ package io.github.sploit.projects.taskmanager.common.services;
 
 import java.util.List;
 
-public interface BService<TDto, ID> {
-    List<TDto> getAll();
-    TDto getById(ID id);
-    TDto add(TDto dto);
-    TDto update(ID id, TDto dto);
+import io.github.sploit.projects.taskmanager.common.dtos.BaseDto;
+
+public interface BService<T extends BaseDto, ID> {
+    List<T> getAll();
+    T getById(ID id);
+    T add(T dto);
+    T update(ID id, T dto);
     Boolean deleteById(ID id);
 }
