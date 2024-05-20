428472cc73351da7625208bed261dcb1f121843c
4sploit <4splo17@gmail.com>
2024-05-20 20:20:00 +0300

    apply common mappings to mapping methods

--- src/main/java/io/github/sploit/projects/taskmanager/common/mappers/BaseMapper.java
+++ src/main/java/io/github/sploit/projects/taskmanager/common/mappers/BaseMapper.java
@@ -1,6 +1,9 @@
 package io.github.sploit.projects.taskmanager.common.mappers;
 
+import org.mapstruct.InheritConfiguration;
+import org.mapstruct.InheritInverseConfiguration;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 
 import io.github.sploit.projects.taskmanager.common.dtos.BaseDto;
 import io.github.sploit.projects.taskmanager.common.entities.BaseEntity;
@@ -10,5 +13,9 @@ public interface BaseMapper<T extends BaseEntity, D extends BaseDto> {
     @Mapping(source = "updateDate", target = "updatedAt")
     D entityToDto(T entity);
 
+    @InheritInverseConfiguration
     T dtoToEntity(D dto);
+
+    @InheritConfiguration
+    void updateEntityFromDto(D dto, @MappingTarget T entity);
 }
