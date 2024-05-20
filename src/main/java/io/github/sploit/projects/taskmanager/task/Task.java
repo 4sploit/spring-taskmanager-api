fe2de5339af832f99b202afa43c06a585016d31a
4sploit <4splo17@gmail.com>
2024-05-20 20:21:11 +0300

    remove unused code

--- src/main/java/io/github/sploit/projects/taskmanager/task/Task.java
+++ src/main/java/io/github/sploit/projects/taskmanager/task/Task.java
@@ -5,13 +5,11 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
 @Getter
 @Setter
-@NoArgsConstructor
 @ToString
 @Entity
 @Table(name = "tasks")
