7951c04117041443ab86fe18fa01fd8f882a20e3
4sploit <4splo17@gmail.com>
2024-05-21 22:59:48 +0300

    add validation to title property

--- src/main/java/io/github/sploit/projects/taskmanager/common/dtos/TitledDto.java
+++ src/main/java/io/github/sploit/projects/taskmanager/common/dtos/TitledDto.java
@@ -2,12 +2,15 @@ package io.github.sploit.projects.taskmanager.common.dtos;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import io.github.sploit.projects.taskmanager.common.constants.DtosValidationErrorMessages;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class TitledDto extends BaseDto {
-    @JsonProperty("title")
+    @NotBlank(message = DtosValidationErrorMessages.NOT_BLANK)
+    @JsonProperty
     private String title;
 }
