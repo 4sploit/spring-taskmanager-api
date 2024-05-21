533846b5c56a0568f5870d1618639cb95425d5dc
4sploit <4splo17@gmail.com>
2024-05-21 22:59:30 +0300

    dto validation infra

---
+++ src/main/java/io/github/sploit/projects/taskmanager/common/constants/ErrorTitles.java
@@ -0,0 +1,7 @@
+package io.github.sploit.projects.taskmanager.common.constants;
+
+public final class ErrorTitles {
+    private ErrorTitles(){}
+
+    public static final String VALIDATION_ERROR_TITLE = "Validation Error";
+}
