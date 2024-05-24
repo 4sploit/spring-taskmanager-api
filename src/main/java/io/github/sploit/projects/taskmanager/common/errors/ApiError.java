8cc4b5b72e85000597db9f4d2b0a05014d76398c
4sploit <4splo17@gmail.com>
2024-05-25 01:09:54 +0300

    add timestamp to api error class

--- src/main/java/io/github/sploit/projects/taskmanager/common/errors/ApiError.java
+++ src/main/java/io/github/sploit/projects/taskmanager/common/errors/ApiError.java
@@ -1,5 +1,6 @@
 package io.github.sploit.projects.taskmanager.common.errors;
 
+import java.time.LocalDateTime;
 import java.util.Set;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
@@ -8,6 +9,9 @@ import lombok.Builder;
 
 @Builder
 public class ApiError {
+    @JsonProperty
+    private final LocalDateTime timestamp = LocalDateTime.now();
+
     @JsonProperty
     private String key;
 
